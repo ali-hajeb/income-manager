674d8ddd93e22f6f30aac73c35533462cfa50042
Ali Hajeb <ali.hajeb@hotmail.com>
2025-12-27 09:50:49 +0330

    fixing search field

--- src/app/containers/HomePanel/index.tsx
+++ src/app/containers/HomePanel/index.tsx
@@ -69,6 +69,9 @@ export default function HomePanel() {
                             setWithdrawalOptions(wOpts);
                         }
                     }
+                    else {
+                        setWithdrawalOptions([]);
+                    }
                 }
             }
         } catch (error) {
